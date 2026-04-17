04d4539c430068328dd89be2863546328c080173
afrith297-sys <afrith297@gmail.com>
2026-04-17 12:33:13 -0300

    Add files via upload

---
+++ index.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    printf("========== RESUME ==========\n\n");
+
+    printf("Name: Afrith\n");
+    printf("Title: Afrith Mark\n\n");
+
+    printf("Objective:\n");
+    printf("To build my skills and gain knowledge while pursuing my studies,\n");
+    printf("and to achieve success through hard work and dedication.\n\n");
+
+    printf("Education:\n");
+    printf("Class: 11th Standard\n");
+    printf("School: Al Ameen School, Kumbakonam\n");
+    printf("Board: State Board\n");
+    printf("Marks:\n");
+    printf("Tamil: 67\nEnglish: 63\nMaths: 89\nScience: 67\nSocial: 68\n\n");
+
+    printf("Skills:\n");
+    printf("- Basic Computer Knowledge\n");
+    printf("- Good Communication\n");
+    printf("- Quick Learner\n");
+    printf("- Teamwork\n\n");
+
+    printf("Personal Details:\n");
+    printf("Date of Birth: 14/11/2010\n");
+    printf("Gender: Male\n");
+    printf("Nationality: Indian\n\n");
+
+    printf("Declaration:\n");
+    printf("I hereby declare that the above information is true to the best of my knowledge.\n\n");
+
+    printf("Signature:\nAfrith\n");
+
+    return 0;
+}
